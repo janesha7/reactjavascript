3029eac1b547be44b8ebe71afcf5ad3f53648587
Janesha Shree <24bct124@kgcas.com>
2026-01-12 14:31:16 +0530

    program2.jsx

--- program2.jsx
+++ program2.jsx
@@ -1,41 +1,36 @@
-import React, { useState } from 'react'
+import { useState } from "react";
 
-const Counter = () => {
-  const[tasks,setTask]=useState([])
-  const[text,setText]=useState("")
-  const addTask=()=>{
-    if(text === "") return;
-    setTask([...tasks,{name:text,completd: false}])
-    setText("")
+function Todolist() {
+  const [tasks, setTask] = useState([])
+  const [text, setText] = useState("")
+
+  const addTask = () => {
+    setTask([...tasks, { name: text, completed: false }])
   }
-  const deleteTask =()=>{
-    setTask(tasks.filter((_,i)=>i!== index))
-  };
+
+  const deleteTask = (index) => {
+    setTask(tasks.filter((_, i) => i !== index))
+  }
   return (
     <>
       <h1>To-Do List</h1>
-      <input 
-      value={text}
-      onChange={(e)=>setText(e.target.value)}
-      placeholder='Enter Task'
-      />
+      <input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Enter Task"></input>
       <button onClick={addTask}>Add</button>
       <ul>
-        {tasks.map((task,index)=>(
-            <li key={index}>
-              <input type="checkbox" />
-             
-                
-                <span>{task.name}</span>
-                <button onClick={()=>deleteTask(index)}>delete</button>
-            </li>
-
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <input type="checkbox"
+              checked={tasks.completed} />
+            <span className={tasks.completed?"done":""}>{task.name}</span>
+            <button onClick={() => deleteTask(index)}>Delete</button>
+          </li>
         ))}
       </ul>
-      
     </>
   )
 }
 
-export default Counter;
-
+export default Todolist;
